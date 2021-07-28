feab3c60b233c86fbc76722833989ac3ac9b1f5f
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-27 22:01:15 -0500

    testing head() built in function and renamed contructors variable to constructors_df to reference that it's a dataframe

--- df.py
+++ df.py
@@ -3,14 +3,20 @@ import pandas as pd
 import settings
 
 ############------------ GLOBAL VARIABLE(S) ------------############
-constructors = pd.read_csv(settings.path_to_csv)
+constructors_df = pd.read_csv(settings.path_to_csv)
 
 
 ############------------ FUNCTION(S) ------------############
 def print_df():
-    print(constructors)
+    print(constructors_df.head(1))
+    # print(constructors)
 
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
     print_df()
+
+    '''
+    constructorId constructorRef     name nationality                                   url
+0              1        mclaren  McLaren     British  http://en.wikipedia.org/wiki/McLaren
+    '''
