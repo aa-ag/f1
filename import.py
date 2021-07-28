248289d9492bc2996d2df8e8a846769b309948df
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-27 21:46:45 -0500

    imported datasets from documentation successfully

--- import.py
+++ import.py
@@ -1,17 +1,23 @@
 ############------------ IMPORTS ------------############
-import json
-# import kaggle
-# from kaggle.api.kaggle_api_extended import KaggleApi
-
+import kaggle
+from kaggle.api.kaggle_api_extended import KaggleApi
 
 ############------------ GLOBAL VARIABLE(S) ------------############
 dataset = 'uciml/iris'
 path = 'datasets/iris'
 
-f = open('kaggle.json', 'r')
-data = json.load(f)
+############------------ FUNCTION(S) ------------############
+def import_data():
+    
+    api = KaggleApi()
+    api.authenticate()
+    
+    api.dataset_download_files(dataset, path)
+    
+    api.dataset_download_file(dataset, 'Iris.csv', path)
+    api.dataset_download_file(dataset, 'database.sqlite', path)
 
-username = data['username']
-key = data['key']
 
-print(username, key)
+############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    import_data()
